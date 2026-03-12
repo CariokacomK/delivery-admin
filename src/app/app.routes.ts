24ce8f1a8f998d7d052965f91f7c802cb2608dde
mariapavezi <maria.luisa.pavezi.08@gmail.com>
2026-03-12 18:16:31 -0300

    feat(categorias-lista): implementar lista de categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { CategoriaCrudComponent } from './pages/categoria-crud/categoria-crud';
+import { CategoriasListaComponent } from './pages/categorias-lista/categorias-lista.component';
 
 export const routes: Routes = [
+  {path: 'categorias', component: CategoriasListaComponent},
+
   { path: 'crud-produto', component: CategoriaCrudComponent },
-  { path: '', redirectTo: '/crud-produto', pathMatch: 'full' }
+  { path: '', redirectTo: '/categorias', pathMatch: 'full' }
 ];
