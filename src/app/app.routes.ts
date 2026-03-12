403970a886d4f6b60bcae067b748527ad74934d1
Gabriel Borges <gobcarioka@gmail.com>
2026-03-12 11:01:18 -0300

    feat(categoria): adicionar categoria crud e localstorage set dos produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CategoriaCrudComponent } from './pages/categoria-crud/categoria-crud';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'crud-produto', component: CategoriaCrudComponent },
+  { path: '', redirectTo: '/crud-produto', pathMatch: 'full' }
+];
