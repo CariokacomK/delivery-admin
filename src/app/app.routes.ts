f0bb996093f0a68c245c2f8e013a59ebc1026676
nicolas <nicolasmaran17@gmail.com>
2026-03-12 19:24:16 -0300

    feature: alterar página de configurações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
-import { CategoriaCrudComponent } from './pages/categoria-crud/categoria-crud';
 
 export const routes: Routes = [
-  { path: 'crud-produto', component: CategoriaCrudComponent },
+  {
+    path: 'crud-produto',
+    loadComponent: () =>
+      import('./pages/categoria-crud/categoria-crud')
+        .then(m => m.CategoriaCrudComponent)
+  },
   { path: '', redirectTo: '/crud-produto', pathMatch: 'full' },
-  {path: 'configuracoes', loadComponent: () =>
-    import('./pages/configuracoes/configuracoes')
-    .then(m => m.Configuracoes)
+  {
+    path: 'configuracoes',
+    loadComponent: () =>
+      import('./pages/configuracoes/configuracoes')
+        .then(m => m.Configuracoes)
   }
 ];
